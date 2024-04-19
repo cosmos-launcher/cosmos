c038ccacb12716661f7885c659a90d3a8fa77b6a
Tanveer Najib <tanveernajib@gmail.com>
2024-04-19 15:48:43 +0100

    Add initial configurations
    - App Icon
    - Screen Rotation
    
    C

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft
+  ]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
 }
 
